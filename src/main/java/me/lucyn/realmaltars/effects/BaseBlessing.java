deb265b4c85a562c1f8e879a5559bc412f780bb6
LucynGoosey <didgeridev@gmail.com>
2024-07-29 04:24:48 -0400

    Added location data to BaseBlessing.java

--- src/main/java/me/lucyn/realmaltars/effects/BaseBlessing.java
+++ src/main/java/me/lucyn/realmaltars/effects/BaseBlessing.java
@@ -1,18 +1,23 @@
 package me.lucyn.realmaltars.effects;
 
+import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 public class BaseBlessing implements Listener {
     public final int id;
     public final String displayName;
-
+    public Location location;
 
 
 
     public BaseBlessing(int id, String displayName) {
         this.id = id;
         this.displayName = displayName;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
     }
 
 
